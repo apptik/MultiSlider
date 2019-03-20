2588b05f751d0a04e9d4834d53c9a79758a97bc0
Maxim Paymushkin <maxim.paymushkin@gmail.com>
2019-03-20 21:55:19 +0300

    fix deprecated import InstrumentationRegistry
    Signed-off-by: Maxim Paymushkin <maxim.paymushkin@gmail.com>

--- example/src/androidTest/java/io/apptik/widget/example/multislider/ExampleTest.java
+++ example/src/androidTest/java/io/apptik/widget/example/multislider/ExampleTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SdkSuppress;
-import androidx.test.InstrumentationRegistry;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.uiautomator.UiCollection;
 import androidx.test.uiautomator.UiDevice;
